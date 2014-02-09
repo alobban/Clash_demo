c9998c65e7c901fe8cb31fa304b12f77d74d84ab
Andrew Lobban <andrewglobban@gmail.com>
2014-02-08 22:59:59 -0600

    Schema Update

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140208215622) do
+
+  create_table "counties", force: true do |t|
+    t.string   "county_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "listings", force: true do |t|
+    t.string   "landlord",                              null: false
+    t.text     "rental_desc",                           null: false
+    t.integer  "bedroom",     default: 0
+    t.float    "bathroom",    default: 1.0
+    t.string   "rate",        default: "Call For Rent"
+    t.string   "phone",       default: "999-9999"
+    t.string   "town",                                  null: false
+    t.integer  "parish_id"
+    t.integer  "county_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "parishes", force: true do |t|
+    t.string   "parish_name"
+    t.integer  "county_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
