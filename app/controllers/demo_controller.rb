622b898c2594950fa4567e2b3c6dd1a148020ed4
Andrew Lobban <andrewglobban@gmail.com>
2014-06-15 16:50:03 -0500

    Update search function

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,7 +3,7 @@ class DemoController < ApplicationController
   end
 
   def results
-  	@properties = Listing.where(:town => params[:town])
+  	@properties = Listing.where(:town => params[:town].capitalize)
   end
 
   def details
